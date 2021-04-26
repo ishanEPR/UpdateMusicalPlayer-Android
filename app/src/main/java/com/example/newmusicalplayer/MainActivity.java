d0acd71fb6c0daac06c8f8043b06ca5e447b8fc9
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-04-26 09:59:45 +0530

    add listview, start musical player

--- app/src/main/java/com/example/newmusicalplayer/MainActivity.java
+++ app/src/main/java/com/example/newmusicalplayer/MainActivity.java
@@ -6,9 +6,21 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,6 +60,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean isMusicPlayerInit;
+
+    private List<String> musicFileList;
+
+    private void addMusicFilesFrom(String dirPath){
+        final File musicDir=new File(dirPath);
+        if (!musicDir.exists()){
+
+            musicDir.mkdir();
+            return;
+        }
+        final File[] files=musicDir.listFiles();
+        for (File file: files){
+            final String path=file.getAbsolutePath();
+            if (path.endsWith(".mp3")){
+                musicFileList.add(path);
+            }
+        }
+    }
+
+    private void fileMusicList(){
+        musicFileList.clear();
+        addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_MUSIC
+        )));
+        addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS
+        )));
+
+    }
+
+    private void playMusicFile(String path){
+        MediaPlayer mp=new MediaPlayer();
+        try {
+            mp.setDataSource(path);
+            mp.prepare();
+            mp.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onResume()
     {
@@ -60,8 +113,66 @@ public class MainActivity extends AppCompatActivity {
 
         if (!isMusicPlayerInit){
             final ListView listView=findViewById(R.id.listView);
+            final TextAdapter textAdapter=new TextAdapter();
+            musicFileList=new ArrayList<>();
+            fileMusicList();
+
+            textAdapter.setData(musicFileList);
+            listView.setAdapter(textAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final String musicFilePath=musicFileList.get(position);
+                    playMusicFile(musicFilePath);
+
+                }
+            });
 
             isMusicPlayerInit=true;
+        }
+    }
+
+
+    class TextAdapter extends BaseAdapter{
+        private List<String> data=new ArrayList<>();
+
+        void setData(List<String> mData){
+            data.clear();
+            data.addAll(mData);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount(){
+            return data.size();
+        }
+        @Override
+        public String getItem(int position){
+            return null;
+        }
+        @Override
+        public long getItemId(int position){
+            return 0;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            if (convertView==null){
+                convertView= LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
+
+                convertView.setTag(new ViewHolder((TextView)convertView.findViewById(R.id.myItem)));
+            }
+            ViewHolder holder=(ViewHolder)convertView.getTag();
+            final String item=data.get(position);
+            holder.info.setText(item.substring(item.lastIndexOf('/')+1));
+            return convertView;
+        }
+        class ViewHolder{
+            TextView info;
+            ViewHolder(TextView mInfo){
+                info=mInfo;
+            }
+
         }
     }
 }
